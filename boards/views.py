e605064bfad64063c4a5a664102abf63854a6636
Wojtek G <w20g16@gmail.com>
2019-02-06 19:07:36 +0100

    board_topics-view

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from boards.models import Board
 
 
@@ -6,3 +7,9 @@ def home(request):
     boards = Board.objects.all()
 
     return render(request, 'home.html', {'boards': boards})
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+
+    return render(request, 'topics.html', {'board': board})
