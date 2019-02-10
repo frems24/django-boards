4f8f743572e551e574ec0f0e4619f143f8d11695
Wojtek G <w20g16@gmail.com>
2019-02-10 20:27:48 +0100

    user-signup-view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse, resolve
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.views import signup
+
+
+class SignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_signup_url_resolves_signup_view(self):
+        view = resolve('/signup/')
+        self.assertEquals(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
--- project/urls.py
+++ project/urls.py
@@ -13,13 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, re_path
+from django.contrib import admin
 
+from accounts import views as account_views
 from boards import views
 
 urlpatterns = [
     re_path(r'^$', views.home, name='home'),
+    re_path(r'^signup/$', account_views.signup, name='signup'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
