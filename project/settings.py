03c48ccc848231530b7ed5e8db3ca652ce81073b
Wojtek G <w20g16@gmail.com>
2019-02-12 18:55:12 +0100

    password-change

--- project/settings.py
+++ project/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-LOGOUT_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- project/urls.py
+++ project/urls.py
@@ -41,6 +41,13 @@ urlpatterns = [
             auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
             name='password_reset_complete'),
 
+    re_path(r'^settings/password/$',
+            auth_views.PasswordChangeView.as_view(template_name='password_change.html'),
+            name='password_change'),
+    re_path(r'^settings/password/done/$',
+            auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'),
+            name='password_change_done'),
+
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
