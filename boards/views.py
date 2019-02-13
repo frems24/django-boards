83007635dbf950a07f58920f2bc97fdccce84677
Wojtek G <w20g16@gmail.com>
2019-02-13 17:42:12 +0100

    start-new-topic-protected

--- boards/views.py
+++ boards/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from boards.forms import NewTopicForm
 from boards.models import Board, Topic, Post
@@ -17,6 +18,7 @@ def board_topics(request, pk):
     return render(request, 'topics.html', {'board': board})
 
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently logged in user
