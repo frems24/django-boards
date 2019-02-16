2a0df940b24007fa4eaf284989a09d67d5155be3
Wojtek G <w20g16@gmail.com>
2019-02-16 20:38:06 +0100

    reusable-pagination-template

--- boards/tests/test_view_topic_posts.py
+++ boards/tests/test_view_topic_posts.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from boards.models import Board, Post, Topic
-from boards.views import topic_posts
+from boards.views import PostListView
 
 
 class TopicPostsTests(TestCase):
@@ -20,4 +20,4 @@ class TopicPostsTests(TestCase):
 
     def test_view_function(self):
         view = resolve('/boards/1/topics/1/')
-        self.assertEquals(view.func, topic_posts)
+        self.assertEquals(view.func.view_class, PostListView)
--- project/urls.py
+++ project/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     re_path(r'^boards/(?P<pk>\d+)/$', views.TopicListView.as_view(), name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
 
-    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.topic_posts, name='topic_posts'),
+    re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/$', views.PostListView.as_view(), name='topic_posts'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/reply/$', views.reply_topic, name='reply_topic'),
     re_path(r'^boards/(?P<pk>\d+)/topics/(?P<topic_pk>\d+)/posts/(?P<post_pk>\d+)/edit/$',
             views.PostUpdateView.as_view(), name='edit_post'),
