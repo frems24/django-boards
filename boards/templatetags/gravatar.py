2628f8bb3884e5ff0e3ad4a6cb3ddec30ce84668
Wojtek G <w20g16@gmail.com>
2019-02-18 18:46:49 +0100

    gravatar

---
+++ boards/templatetags/gravatar.py
@@ -0,0 +1,19 @@
+import hashlib
+from urllib.parse import urlencode
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter
+def gravatar(user):
+    email = user.email.lower().encode('utf-8')
+    default = 'mm'
+    size = 64
+    md5 = hashlib.md5(email).hexdigest()
+    params = urlencode({'d': default, 's': str(size)})
+    url = f'https://www.gravatar.com/avatar/{md5}?{params}'
+
+    return url
