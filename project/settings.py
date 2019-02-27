afedc0eeefa360af055eed2636b20224e14726b9
Wojtek G <w20g16@gmail.com>
2019-02-27 18:48:24 +0100

    Static_root changed to staticfiles

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
