6b1f7b09784a690463acbd2958817379644e19f6
Wojtek G <w20g16@gmail.com>
2019-02-27 19:23:57 +0100

    gunicorn_start script

---
+++ gunicorn_start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+NAME="django_boards"
+DIR=/home/wg/boards
+USER=wg
+GROUP=wg
+WORKERS=3
+BIND=unix:/home/wg/run/boards.socket
+DJANGO_SETTINGS_MODULE=project.settings
+DJANGO_WSGI_MODULE=project.wsgi
+LOG_LEVEL=error
+
+cd ${DIR}
+source /home/wg/.local/share/virtualenvs/boards-o-M7WUJG/bin/activate
+
+export DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE}
+export PYTHONPATH=${DIR}:${PYTHONPATH}
+
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+    --name ${NAME} \
+    --workers ${WORKERS} \
+    --user=${USER} \
+    --group=${GROUP} \
+    --bind=${BIND} \
+    --log-level=${LOG_LEVEL} \
+    --log-file=-
